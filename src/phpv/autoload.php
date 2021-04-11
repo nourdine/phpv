b3bc4efaf09e8686d2812d4e5c36510b33ae88b3
nourdine <ftestolin@gmail.com>
2021-04-11 10:59:55 +0200

    dev

--- src/phpv/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-spl_autoload_register(function($cn) {
-      $pathBits = preg_split("/\\\\/", $cn);
-      $fullPath = dirname(__DIR__) . "/" . implode("/", $pathBits) . ".php";
-      if (is_readable($fullPath)) {
-         require_once($fullPath);
-      }
-   }, true);
